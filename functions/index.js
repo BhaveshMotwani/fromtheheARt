58a8f5a8f524aa4e7c7901ad7df5a1afa1098863
simonguozirui <simonguozirui@gmail.com>
2018-03-14 16:15:34 -0700

    add two function endpoints and layout database structure

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const functions = require('firebase-functions');
 
 // The Firebase Admin SDK to access the Firebase Realtime Database.
@@ -6,7 +8,35 @@ admin.initializeApp(functions.config().firebase);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+
+
 exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
+});
+
+
+
+// Take the text parameter passed to this HTTP endpoint and insert it into the
+// Realtime Database under the path /messages/:pushId/original
+exports.updateGyroZ1 = functions.https.onRequest((req, res) => {
+  // Grab the text parameter.
+  const user1Val = req.query.user1;
+  // Push the new message into the Realtime Database using the Firebase Admin SDK.
+  return admin.database().ref('/gyros').set({user1: user1Val}).then((snapshot) => {
+    // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+    return res.redirect(303, snapshot.ref);
+  });
+});
+
+
+// Take the text parameter passed to this HTTP endpoint and insert it into the
+// Realtime Database under the path /messages/:pushId/original
+exports.updateGyroZ2 = functions.https.onRequest((req, res) => {
+  // Grab the text parameter.
+  const user2Val = req.query.user2;
+  // Push the new message into the Realtime Database using the Firebase Admin SDK.
+  return admin.database().ref('/gyros').set({user2: user2Val}).then((snapshot) => {
+    // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+    return res.redirect(303, snapshot.ref);
+  });
 });
