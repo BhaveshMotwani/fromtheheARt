912d34cacb7fd27da43dbef7428ac8bfbc0537ce
simonguozirui <simonguozirui@gmail.com>
2018-03-13 21:52:11 -0700

    add accerlation and gyro z axis variabels

--- Assets/GyroControl.cs
+++ Assets/GyroControl.cs
@@ -27,15 +27,32 @@ public class GyroControl : MonoBehaviour {
         GUI.skin.label.fontSize = Screen.width / 40;
 
         GUILayout.Label("Orientation: " + Screen.orientation);
-        GUILayout.Label("input.gyro.attitude: " + Input.gyro.attitude);
-		GUILayout.Label("intput.gyro.euler: " + Input.gyro.attitude.eulerAngles.ToString());
+        //GUILayout.Label("input.gyro.attitude: " + Input.gyro.attitude);
+		GUILayout.Label("input.gyro.euler: " + Input.gyro.attitude.eulerAngles.ToString());
+		GUILayout.Label ("input.acceleration: " + Input.acceleration.ToString());
     }
 
 
 	// // Update is called once per frame
 	private void Update() {
+//		float speed = 10.0F;
 		transform.rotation = GyroToUnity(Input.gyro.attitude);
+//		Vector3 dir = Vector3.zero;
+//		dir.x = -Input.acceleration.y;
+//		dir.z = Input.acceleration.x;
+//		if (dir.sqrMagnitude > 1)
+//			dir.Normalize();
+//
+//		dir *= Time.deltaTime;
+//		transform.Translate(dir * speed);
 	}	
+
+	private void Data(){
+		float gyroZ = Input.gyro.attitude.eulerAngles.z;
+		float accZ = Input.acceleration.z;
+		Debug.Log(gyroZ);
+		Debug.Log(accZ);
+	}
 
 	private static Quaternion GyroToUnity(Quaternion q)
     {
